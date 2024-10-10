cbe9bf7ea30afd1672c06a3ee50050ca9cfdcdf5
hugo-syn <hugo.vincent@synacktiv.com>
2024-10-10 17:38:48 +0200

    fix: replace for sh and not bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,21 +11,22 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 # Initialize the base command
 COMMAND="octoscan scan ."
 
+
 # Add options if the corresponding environment variables are defined
 if [ -n "$INPUT_DEBUG_RULES" ]; then
-    COMMAND+=" --debug-rules"
+    COMMAND="$COMMAND --debug-rules"
 fi
 
 if [ -n "$INPUT_FILTER_TRIGGERS" ]; then
-    COMMAND+=" --filter-triggers \"$INPUT_FILTER_TRIGGERS\""
+    COMMAND="$COMMAND --filter-triggers \"$INPUT_FILTER_TRIGGERS\""
 fi
 
 if [ -n "$INPUT_FILTER_RUN" ]; then
-    COMMAND+=" --filter-run"
+    COMMAND="$COMMAND --filter-run"
 fi
 
 if [ -n "$INPUT_IGNORE" ]; then
-    COMMAND+=" --ignore \"$INPUT_IGNORE\""
+    COMMAND="$COMMAND --ignore \"$INPUT_IGNORE\""
 fi
 
 # --disable-rules and --enable-rules are exclusive
@@ -35,20 +36,20 @@ if [ -n "$INPUT_DISABLE_RULES" ] && [ -n "$INPUT_ENABLE_RULES" ]; then
 fi
 
 if [ -n "$INPUT_DISABLE_RULES" ]; then
-    COMMAND+=" --disable-rules \"$INPUT_DISABLE_RULES\""
+    COMMAND="$COMMAND --disable-rules \"$INPUT_DISABLE_RULES\""
 elif [ -n "$INPUT_ENABLE_RULES" ]; then
-    COMMAND+=" --enable-rules \"$INPUT_ENABLE_RULES\""
+    COMMAND="$COMMAND --enable-rules \"$INPUT_ENABLE_RULES\""
 fi
 
 if [ -n "$INPUT_CONFIG_FILE" ]; then
-    COMMAND+=" --config-file \"$INPUT_CONFIG_FILE\""
+    COMMAND="$COMMAND --config-file \"$INPUT_CONFIG_FILE\""
 fi
 
 # Print the constructed command for debugging
 echo "Constructed command: $COMMAND"
 
 # Execute the command
-eval $COMMAND | reviewdog -efm="%f:%l:%c: %m" \
+sh -c "$COMMAND" | reviewdog -efm="%f:%l:%c: %m" \
       -name="octoscan" \
       -reporter="${INPUT_REPORTER}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
