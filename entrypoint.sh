67a25af758ce35b19536cf9e5c5394a10b1e0df5
hugo-syn <hugo.vincent@synacktiv.com>
2024-10-10 17:05:49 +0200

    feat: initial commit might fail

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,54 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-misspell -locale="${INPUT_LOCALE}" . \
-  | reviewdog -efm="%f:%l:%c: %m" \
-      -name="linter-name (misspell)" \
-      -reporter="${INPUT_REPORTER:-github-pr-check}" \
+# Initialize the base command
+COMMAND="octoscan scan ."
+
+# Add options if the corresponding environment variables are defined
+if [ -n "$INPUT_DEBUG_RULES" ]; then
+    COMMAND+=" --debug-rules"
+fi
+
+if [ -n "$INPUT_FILTER_TRIGGERS" ]; then
+    COMMAND+=" --filter-triggers \"$INPUT_FILTER_TRIGGERS\""
+fi
+
+if [ -n "$INPUT_FILTER_RUN" ]; then
+    COMMAND+=" --filter-run"
+fi
+
+if [ -n "$INPUT_IGNORE" ]; then
+    COMMAND+=" --ignore \"$INPUT_IGNORE\""
+fi
+
+# --disable-rules and --enable-rules are exclusive
+if [ -n "$INPUT_DISABLE_RULES" ] && [ -n "$INPUT_ENABLE_RULES" ]; then
+    echo "Error: Both --disable-rules and --enable-rules cannot be set simultaneously."
+    exit 1
+fi
+
+if [ -n "$INPUT_DISABLE_RULES" ]; then
+    COMMAND+=" --disable-rules \"$INPUT_DISABLE_RULES\""
+elif [ -n "$INPUT_ENABLE_RULES" ]; then
+    COMMAND+=" --enable-rules \"$INPUT_ENABLE_RULES\""
+fi
+
+if [ -n "$INPUT_CONFIG_FILE" ]; then
+    COMMAND+=" --config-file \"$INPUT_CONFIG_FILE\""
+fi
+
+# Print the constructed command for debugging
+echo "Constructed command: $COMMAND"
+
+# Execute the command
+eval $COMMAND | reviewdog -efm="%f:%l:%c: %m" \
+      -name="octoscan" \
+      -reporter="${INPUT_REPORTER}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
       ${INPUT_REVIEWDOG_FLAGS}
+
+exit_code=$?
+
+exit $exit_code
